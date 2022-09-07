068140c0cc20dde7d33bddd3fdb8abb461caa402
Henry Sanchez <henrysanchezcarvajal@gmail.com>
2022-09-06 20:42:42 -0400

    feat: se agrega conexion a mongo

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const app = require('./app');
+const mongoose = require('mongoose');
 
 const port = process.env.PORT;
+const dbUrl = process.env.DB_URL;
+const dbName = process.env.DB_NAME;
 
+mongoose.connect(`${dbUrl}/${dbName}`)
+  .then((con) => {
+    console.log('Conectado a Mongo', con.connection.host);
+  });
 app.listen(port, () => {
   console.log(`Servidor iniciado en puerto ${port}`);
  });
