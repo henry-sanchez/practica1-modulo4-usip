9894566edf60f32c028847f3eec2a58d2d7e390a
Henry Sanchez <henrysanchezcarvajal@gmail.com>
2022-09-08 20:52:15 -0400

    feat: se agrega clase para manejo de errores

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const productRouter = require('./routers/productRoutes');
+const MyError = require('./utils/MyError');
 
 const app = express();
 
@@ -14,5 +15,24 @@ app.use((req, res, next) => {
 
 // routes
 app.use('/api/v1/products', productRouter);
+
+app.all('*', (req, res, next) => {
+  next(new MyError('route not found', 404));
+});
+
+// recibimos como primer parámetro instancia de MyError
+app.use((err, req, res, next) => {
+  if (process.env.NODE_ENV === 'development') {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: 'err.message',
+    });
+  }
+});
 
 module.exports = app;
