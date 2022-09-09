2143129e639c51cccd3143fb9b5b0201ecc7b600
Henry Sanchez <henrysanchezcarvajal@gmail.com>
2022-09-08 21:36:47 -0400

    feat: se manejan errores en inicio de servidor

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-const app = require('./app');
 const mongoose = require('mongoose');
 
+process.on('uncaughtException', err => {
+  console.error('uncaughtException ' + err.message);
+  console.error('Shutting down');
+  process.exit(1);
+});
+
+const app = require('./app');
 const port = process.env.PORT;
 const dbUrl = process.env.DB_URL;
 const dbName = process.env.DB_NAME;
@@ -9,6 +15,14 @@ mongoose.connect(`${dbUrl}/${dbName}`)
   .then((con) => {
     console.log('Conectado a Mongo', con.connection.host);
   });
-app.listen(port, () => {
+const server = app.listen(port, () => {
   console.log(`Servidor iniciado en puerto ${port}`);
- });
+});
+
+process.on('unhandledRejection', err => {
+  console.error('unhandledRejection ' + err.message);
+  console.error('Shutting down');
+  server.close(() => {
+    process.exit(1);
+  });
+});
